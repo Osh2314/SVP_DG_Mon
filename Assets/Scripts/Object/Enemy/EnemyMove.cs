d1a03cd87a2af706b3d9a1c9c8a2ad8ab4212aec
Osh2314 <osh231426@gmail.com>
2019-07-25 12:35:16 +0900

    프로토타입 - 2

---
+++ Assets/Scripts/Object/Enemy/EnemyMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+    Rigidbody2D rigid;
+
+    public float speed;
+    private bool cMove = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cMove == true)
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+           // rigid.velocity(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    public void SetcMove(bool setMove)
+    {
+        cMove = setMove;
+    }
+}
